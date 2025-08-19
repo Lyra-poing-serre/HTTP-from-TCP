92739521de9d6fd6c21ccabc45c6ff50dc41b8a5
Lyra-poing-serre <23309908+Lyra-poing-serre@users.noreply.github.com>
2025-08-19 14:33:46 +0200

    goroutine

--- main.go
+++ main.go
@@ -6,24 +6,48 @@ import (
 	"io"
 	"log"
 	"os"
+	"strings"
 )
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	ch := make(chan string)
+
+	go func() {
+		defer f.Close()
+		defer close(ch)
+
+		buf := make([]byte, 8)
+		line := ""
+		for {
+			n, err := f.Read(buf)
+			if err != nil {
+				if errors.Is(err, io.EOF) {
+					break
+				}
+				log.Fatalf("an error occured while reading the file : %s\n", err.Error())
+			}
+			s := strings.Split(string(buf[:n]), "\n")
+			line += s[0]
+			if len(s) > 1 {
+				ch <- line
+				line = strings.Join(s[1:], "")
+			}
+		}
+		ch <- line
+	}()
+
+	return ch
+}
 
 func main() {
 	f, err := os.Open("./messages.txt")
 	if err != nil {
 		log.Fatalf("an error occured when openning the file : %s\n", err.Error())
 	}
-	defer f.Close()
 
-	buf := make([]byte, 8)
-	for {
-		n, err := f.Read(buf)
-		if err != nil {
-			if errors.Is(err, io.EOF) {
-				break
-			}
-			log.Fatalf("an error occured while reading the file : %s\n", err.Error())
-		}
-		fmt.Printf("read: %s\n", string(buf[:n]))
+	ch := getLinesChannel(f)
+
+	for str := range ch {
+		fmt.Printf("read: %s\n", str)
 	}
 }
