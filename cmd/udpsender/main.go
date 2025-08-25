c176433622da72a6e1bdca84fb2595c44a1fd1c4
Lyra-poing-serre <23309908+Lyra-poing-serre@users.noreply.github.com>
2025-08-25 13:47:06 +0200

    UDP ok

--- cmd/udpsender/main.go
+++ cmd/udpsender/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io"
 	"log"
 	"net"
+	"os"
 )
 
 func main() {
@@ -15,5 +20,23 @@ func main() {
 		log.Fatalf("an error occured when setting up the UDP connection: %s\n", err.Error())
 	}
 	defer con.Close()
+
+	bufReader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("> ")
+		str, err := bufReader.ReadString(byte('\n'))
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				log.Println("End detected, leaving for loop.")
+				break
+			} else {
+				log.Fatalf("an error occured when setting up the UDP connection: %s\n", err.Error())
+			}
+		}
+		_, err = con.Write([]byte(str))
+		if err != nil {
+			log.Fatalf("an error occured when writing to the UDP connection: %s\n", err.Error())
+		}
+	}
 
 }
