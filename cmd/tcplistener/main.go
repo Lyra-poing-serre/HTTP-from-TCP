641b299c679c385b1c70773c505075fd2bdaa188
lyra <23309908+Lyra-poing-serre@users.noreply.github.com>
2025-08-26 19:47:20 +0200

    request line done

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -1,48 +1,21 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"io"
 	"log"
 	"net"
-	"strings"
+
+	request "github.com/Lyra-poing-serre/HTTP-from-TCP/internal/request"
 )
-
-func getLinesChannel(f io.ReadCloser) <-chan string {
-	ch := make(chan string)
-
-	go func() {
-		defer f.Close()
-		defer close(ch)
-
-		buf := make([]byte, 8)
-		line := ""
-		for {
-			n, err := f.Read(buf)
-			if err != nil {
-				if errors.Is(err, io.EOF) {
-					break
-				}
-				log.Fatalf("an error occured while reading the file : %s\n", err.Error())
-			}
-			s := strings.Split(string(buf[:n]), "\n")
-			line += s[0]
-			if len(s) > 1 {
-				ch <- line
-				line = strings.Join(s[1:], "")
-			}
-		}
-		ch <- line
-	}()
-
-	return ch
-}
 
 func main() {
 	ln, err := net.Listen("tcp", ":42069")
 	if err != nil {
-		log.Fatalf("an error occured when setting up the TCP listener : %s\n", err.Error())
+		log.Printf(
+			"an error occured when setting up the TCP listener : %s\n",
+			err.Error(),
+		)
+		return
 	}
 	defer ln.Close()
 	fmt.Println("Listening for TCP traffic on :42069")
@@ -50,15 +23,27 @@ func main() {
 	for {
 		con, err := ln.Accept()
 		if err != nil {
-			log.Fatalf("an error occured when setting up the TCP listener : %s\n", err.Error())
+			log.Printf(
+				"an error occured when setting up the TCP listener : %s\n",
+				err.Error(),
+			)
+			return
 		}
 		fmt.Println("A new connection has been accepted.")
 
-		ch := getLinesChannel(con)
-
-		for str := range ch {
-			fmt.Println(str)
+		r, err := request.RequestFromReader(con)
+		if err != nil {
+			log.Printf(
+				"an error occured while parsing the reqeust: %s\n",
+				err.Error(),
+			)
+			return
 		}
+
+		fmt.Printf("Request line:\n- Method: %s\n", r.RequestLine.Method)
+		fmt.Printf("- Target: %s\n", r.RequestLine.RequestTarget)
+		fmt.Printf("- Version: %s\n", r.RequestLine.HttpVersion)
+
 		fmt.Println("The connection has been closed.")
 	}
 }
