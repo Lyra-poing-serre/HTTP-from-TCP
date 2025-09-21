612a6b58c1d2169bf67c6f2d37b4e8fc07d3fce0
Lyra <23309908+Lyra-poing-serre@users.noreply.github.com>
2025-09-21 18:21:09 +0200

    video

--- cmd/httpserver/main.go
+++ cmd/httpserver/main.go
@@ -21,134 +21,9 @@ import (
 	"github.com/Lyra-poing-serre/HTTP-from-TCP/internal/tools"
 )
 
-const (
-	port           = 42069
-	badRequestHTML = `
-<html>
-  <head>
-    <title>400 Bad Request</title>
-  </head>
-  <body>
-    <h1>Bad Request</h1>
-    <p>Your request honestly kinda sucked.</p>
-  </body>
-</html>
-`
-	internalServerError = `<html>
-  <head>
-    <title>500 Internal Server Error</title>
-  </head>
-  <body>
-    <h1>Internal Server Error</h1>
-    <p>Okay, you know what? This one is on me.</p>
-  </body>
-</html>`
-	okResponse = `<html>
-  <head>
-    <title>200 OK</title>
-  </head>
-  <body>
-    <h1>Success!</h1>
-    <p>Your request was an absolute banger.</p>
-  </body>
-</html>`
-)
-
 func main() {
-	server, err := server.Serve(
-		port,
-		func(w response.Writer, req *request.Request) {
-			if strings.HasPrefix(req.RequestLine.RequestTarget, "/httpbin") {
-				// ctx timeout todo
-				chHttpbin := make(chan []byte)
-				target := fmt.Sprintf(
-					"%s%s",
-					"https://httpbin.org",
-					strings.TrimPrefix(
-						req.RequestLine.RequestTarget,
-						"/httpbin",
-					),
-				)
-				go func() {
-					resp, err := http.Get(target)
-					if err != nil {
-						fmt.Println(err)
-						return
-					}
-					defer close(chHttpbin)
-					defer resp.Body.Close()
-					for {
-						b := make([]byte, tools.ChunkSize)
-						n, err := resp.Body.Read(b)
-						if n > 0 {
-							chHttpbin <- b[:n]
-						}
-						if errors.Is(err, io.EOF) {
-							return
-						}
-					}
-				}()
-
-				err := w.WriteStatusLine(response.StatusOK)
-				if err != nil {
-					fmt.Println(err)
-					return
-				}
-				h := headers.NewHeaders()
-				h.Set("Content-Type", "application/json")
-				h.Set("Transfer-Encoding", "chunked")
-				h.Set("Trailer", "X-Content-SHA256")
-				h.Set("Trailer", "X-Content-Length")
-				err = w.WriteHeaders(h)
-				if err != nil {
-					fmt.Println(err)
-					return
-				}
-
-				body := []byte{}
-				for chunk := range chHttpbin {
-					body = append(body, chunk...)
-					_, err = w.WriteChunkedBody(chunk)
-					if err != nil {
-						fmt.Println(err)
-						return
-					}
-				}
-				_, err = w.WriteChunkedBodyDone()
-				if err != nil {
-					fmt.Println(err)
-					return
-				}
-
-				sum := sha256.Sum256(body)
-				h.Set("X-Content-SHA256", hex.EncodeToString(sum[:]))
-				h.Set("X-Content-Length", strconv.Itoa(len(body)))
-				err = w.WriteTrailers(h)
-				if err != nil {
-					fmt.Println(err)
-				}
-				return
-			} else {
-				err := w.WriteStatusLine(response.StatusOK)
-				if err != nil {
-					fmt.Println(err)
-					return
-				}
-				h := response.GetDefaultHeaders(len(okResponse))
-				err = w.WriteHeaders(h)
-				if err != nil {
-					fmt.Println(err)
-					return
-				}
-				_, err = w.WriteBody([]byte(okResponse))
-				if err != nil {
-					fmt.Println(err)
-					return
-				}
-				return
-			}
-		},
-	)
+	const port = 42069
+	server, err := server.Serve(port, handler)
 	if err != nil {
 		log.Fatalf("Error starting server: %v", err)
 	}
@@ -159,4 +34,210 @@ func main() {
 	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
 	<-sigChan
 	log.Println("Server gracefully stopped")
+}
+
+func handler(w response.Writer, req *request.Request) {
+	if req.RequestLine.Method != "GET" {
+		okHandler(w, req)
+	}
+	if strings.HasPrefix(req.RequestLine.RequestTarget, "/httpbin") {
+		proxyHandler(w, req)
+	} else if req.RequestLine.RequestTarget == "/yourproblem" {
+		badRequestHandler(w, req)
+	} else if req.RequestLine.RequestTarget == "/myproblem" {
+		internalErrorHandler(w, req)
+	} else if req.RequestLine.RequestTarget == "/video" {
+		videoHandler(w, req)
+	} else {
+		okHandler(w, req)
+	}
+}
+
+func videoHandler(w response.Writer, _ *request.Request) {
+	err := w.WriteStatusLine(response.StatusOK)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	data, err := os.ReadFile("assets/vim.mp4")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	h := headers.NewHeaders()
+	h.Set("Content-Type", "video/mp4")
+	h.Set("Cpntent-Lenght", strconv.Itoa(len(data)))
+	err = w.WriteHeaders(h)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	_, err = w.WriteBody(data)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	w.WriteBody([]byte(tools.CRLF))
+}
+
+func proxyHandler(w response.Writer, req *request.Request) {
+	// ctx timeout todo
+	chHttpbin := make(chan []byte)
+	target := fmt.Sprintf(
+		"%s%s",
+		"https://httpbin.org",
+		strings.TrimPrefix(
+			req.RequestLine.RequestTarget,
+			"/httpbin",
+		),
+	)
+	go func() {
+		resp, err := http.Get(target)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer close(chHttpbin)
+		defer resp.Body.Close()
+		for {
+			b := make([]byte, tools.ChunkSize)
+			n, err := resp.Body.Read(b)
+			if n > 0 {
+				chHttpbin <- b[:n]
+			}
+			if errors.Is(err, io.EOF) {
+				return
+			}
+		}
+	}()
+
+	err := w.WriteStatusLine(response.StatusOK)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	h := headers.NewHeaders()
+	h.Set("Content-Type", "application/json")
+	h.Set("Transfer-Encoding", "chunked")
+	h.Set("Trailer", "X-Content-SHA256")
+	h.Set("Trailer", "X-Content-Length")
+	err = w.WriteHeaders(h)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	body := []byte{}
+	for chunk := range chHttpbin {
+		body = append(body, chunk...)
+		_, err = w.WriteChunkedBody(chunk)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+	_, err = w.WriteChunkedBodyDone()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	sum := sha256.Sum256(body)
+	h.Set("X-Content-SHA256", hex.EncodeToString(sum[:]))
+	h.Set("X-Content-Length", strconv.Itoa(len(body)))
+	err = w.WriteTrailers(h)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func badRequestHandler(w response.Writer, _ *request.Request) {
+	const badRequestHTML = `
+<html>
+  <head>
+    <title>400 Bad Request</title>
+  </head>
+  <body>
+    <h1>Bad Request</h1>
+    <p>Your request honestly kinda sucked.</p>
+  </body>
+</html>
+`
+	err := w.WriteStatusLine(response.StatusBadRequest)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	h := response.GetDefaultHeaders(len(badRequestHTML))
+	err = w.WriteHeaders(h)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	_, err = w.WriteBody([]byte(badRequestHTML))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	w.WriteBody([]byte(tools.CRLF))
+}
+
+func internalErrorHandler(w response.Writer, _ *request.Request) {
+	const internalServerError = `<html>
+  <head>
+    <title>500 Internal Server Error</title>
+  </head>
+  <body>
+    <h1>Internal Server Error</h1>
+    <p>Okay, you know what? This one is on me.</p>
+  </body>
+</html>`
+	err := w.WriteStatusLine(response.StatusInternalServerError)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	h := response.GetDefaultHeaders(len(internalServerError))
+	err = w.WriteHeaders(h)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	_, err = w.WriteBody([]byte(internalServerError))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	w.WriteBody([]byte(tools.CRLF))
+}
+
+func okHandler(w response.Writer, _ *request.Request) {
+	const okResponse = `<html>
+  <head>
+    <title>200 OK</title>
+  </head>
+  <body>
+    <h1>Success!</h1>
+    <p>Your request was an absolute banger.</p>
+  </body>
+</html>`
+	err := w.WriteStatusLine(response.StatusOK)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	h := response.GetDefaultHeaders(len(okResponse))
+	err = w.WriteHeaders(h)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	_, err = w.WriteBody([]byte(okResponse))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	w.WriteBody([]byte(tools.CRLF))
 }
